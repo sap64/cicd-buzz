a5019cddb504864344338bc3e67381628005e6bf
Sagar <sagar2593@gmail.com>
2021-02-24 00:59:49 -0500

    Changed to 5000

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ def generate_buzz():
     return page
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 8080)))
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
